9839c0730d00af01c8a39fa8b7085772e8a86139
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2021-08-18 04:09:38 +0100

    Refactored CalendarHelper into Calendar Manager, it is now not static, but does include a dfew static methods, so that it can be called from elswhere in the program.

---
+++ TeacherPlanner/Helpers/CalendarManager.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using TeacherPlanner.Constants;
+using TeacherPlanner.Login.Models;
+
+namespace TeacherPlanner.Helpers
+{
+    public class CalendarManager : ObservableObject
+    {
+        // Fields
+        private DateTime _today;
+        private string _currentDateLeftString;
+        private string _currentDateRightString;
+        private bool _datesAreNeighbours = true;
+
+        public CalendarManager(string academicYear)
+        {
+            CurrentAcademicYear = academicYear.Substring(0, 4);
+            CurrentDates = new DateTime[2];
+            
+            var startOfYearDate = GetStartOfYearDateLimit(CurrentAcademicYear);
+            StartOfYearDateLimit = startOfYearDate;
+            EndOfYearDateLimit = GetEndOfYearDateLimit(CurrentAcademicYear);
+
+            // MUST be delcared After setting the StartOfYearDateLimit and EndOfYearDateLimit
+            Today = DateTime.Today;
+
+            CurrentDateLeft = IsAcademicYearNow(CurrentAcademicYear) ? Today : startOfYearDate;
+        }
+
+        // Properties
+        public string CurrentAcademicYear { get; set; }
+        public DateTime[] CurrentDates { get; set; }
+
+        public DateTime Today
+        {
+            get => _today;
+            set
+            {
+                if (value <= StartOfYearDateLimit)
+                    _today = StartOfYearDateLimit;
+                else if (value >= EndOfYearDateLimit)
+                    _today = EndOfYearDateLimit;
+                else
+                    _today = value;
+                TodayString = _today.ToString(Formats.DateHeadingFormat);
+                Tomorrow = AdvanceDate(_today, 1);
+                TomorrowString = Tomorrow.ToString(Formats.DateHeadingFormat);
+            }
+        }
+
+        public bool DatesAreNeighbours 
+        { 
+            get => _datesAreNeighbours; 
+            set => RaiseAndSetIfChanged(ref _datesAreNeighbours, value); 
+        }
+
+        public string TodayString { get; private set; }
+
+        public DateTime Tomorrow { get; private set; }
+
+        public string TomorrowString { get; private set; }
+
+        public DateTime CurrentDateLeft
+        {
+            get => CurrentDates[0];
+            set
+            {
+                CurrentDates[0] = value;
+                _currentDateLeftString = CurrentDates[0].ToString(Formats.DateHeadingFormat);
+                if (DatesAreNeighbours) CurrentDateRight = AdvanceDate(CurrentDates[0], 1);
+
+            }
+        }
+
+        public DateTime CurrentDateRight
+        {
+            get => CurrentDates[1];
+            set
+            {
+                CurrentDates[1] = value;
+                _currentDateRightString = CurrentDates[1].ToString(Formats.DateHeadingFormat);
+                if (DatesAreNeighbours && _currentDateLeftString != AdvanceDate(CurrentDates[1], -1).ToString(Formats.DateHeadingFormat))
+                    CurrentDateLeft = AdvanceDate(CurrentDates[1], -1);
+            }
+        }
+
+        public DateTime StartOfYearDateLimit { get; private set; }
+        public DateTime EndOfYearDateLimit { get; private set; }
+
+        // Static Methods
+        public static string GetStartingYearOfAcademicYear(DateTime date)
+        {
+            var day = Int32.Parse(date.ToString("dd"));
+            var month = Int32.Parse(date.ToString("MM"));
+            var year = Int32.Parse(date.ToString("yyyy"));
+            if ((month == 8 && day <= 15) || month > 1 && month < 8)
+                return (year - 1).ToString();
+            else
+                return year.ToString();
+        }
+
+        public static int GetWeek(DateTime date)
+        {
+            var filepath = Path.Combine(FileHandlingHelper.LoggedInUserDataPath, Filenames.TimetableWeeks);
+            var weekdata = FileHandlingHelper.ReadDataFromCSVFile(filepath);
+            for (var i = 0; i < weekdata.Length; i++)
+            {
+                var week = weekdata[i];
+                var weekstring = week[0].Split("/");
+                DateTime currentWeek = new DateTime(Int32.Parse(weekstring[0]), Int32.Parse(weekstring[1]), Int32.Parse(weekstring[2]));
+                DateTime nextWeek = currentWeek.AddDays(7);
+                if (date >= currentWeek && date < nextWeek)
+                {
+                    if (week[1] == "True")
+                        return 1;
+                    else if (week[2] == "True")
+                        return 2;
+                    else if (week[3] == "True")
+                        return 3;
+                    else
+                        return 0;
+                }
+            }
+            return 0;
+        }
+        public static bool IsAcademicYearNow(string year)
+        {
+            var thisAcademicYearString = GetStartingYearOfAcademicYear(DateTime.Today);
+            return year == thisAcademicYearString;
+        }
+
+        public static DateTime GetStartOfYearDateLimit(string AcademicYearString)
+        {
+            var yearInt = Int32.Parse(AcademicYearString);
+            var month = 8;
+            var day = 21;
+            var date = new DateTime(yearInt, month, day);
+            while (date.ToString("dddd").ToLower() != "monday")
+            {
+                day += 1;
+                date = date.AddDays(1);
+            }
+            return new DateTime(yearInt, month, day);
+        }
+
+        public static DateTime GetEndOfYearDateLimit(string AcademicYearString)
+        {
+            var yearInt = Int32.Parse(AcademicYearString) + 1;
+            var month = 8;
+            var day = 7;
+            var date = new DateTime(yearInt, month, day);
+            while (date.ToString("dddd").ToLower() != "friday")
+            {
+                day += 1;
+                date = date.AddDays(1);
+            }
+            return new DateTime(yearInt, month, day);
+        }
+
+        // Non-Static Methods
+        public void ChangeCurrentDate(int days)
+        {
+            if (days < 0) CurrentDateLeft = AdvanceDate(CurrentDateLeft, days);
+            else CurrentDateRight = AdvanceDate(CurrentDateRight, days);
+        }
+        
+        
+        private DateTime AdvanceDate(DateTime date, int days)
+        {
+            DateTime newDate = date.AddDays(days);
+            var extraDays = 0;
+            if (newDate.DayOfWeek == DayOfWeek.Saturday)
+            {
+                extraDays = days < 0 ? -1 : 2;
+            }
+            else if (newDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                extraDays = days < 0 ? -2 : 1;
+            }
+            newDate = newDate.AddDays(extraDays);
+            return newDate > StartOfYearDateLimit.AddDays(-1) && newDate < EndOfYearDateLimit.AddDays(1) ? newDate : date;
+            
+        }
+    }
+}
