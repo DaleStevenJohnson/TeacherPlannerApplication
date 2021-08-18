5e0b0d8e964ca8a733dfe0290685735334149d88
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2021-08-18 04:08:39 +0100

    Added Constants namespace along with 3 constants classes to better help show intention and keep consistency throughout the application

---
+++ TeacherPlanner/Constants/Filenames.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherPlanner.Constants
+{
+    public static class Filenames
+    {
+        public static string TimetableWeeks { get; set; } = "TimetableWeeks.csv";
+    }
+}
---
+++ TeacherPlanner/Constants/Formats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeacherPlanner.Constants
+{
+    public static class Formats
+    {
+        //TODO merge all formats into this class
+        public static string DateHeadingFormat { get; } = "dddd, dd MMMM";
+        public static string FullDateFormat { get; } = "yyyyMMdd";
+        public static string FullDayNameFormat { get; } = "dddd";
+        public static string FullMonthNameFormat { get; } = "MMMM";
+        public static string FullMonthNumberFormat { get; } = "MM";
+        public static string FullYearNumberFormat { get; } = "yyyy";
+        public static string FullMonthNameAndFullYearNumberFormat = FullMonthNameFormat + " " + FullYearNumberFormat;
+    }
+}
